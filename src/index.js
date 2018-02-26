0e3dabe5202750d8545340154c115ad6917919b6
Til Blechschmidt <til@blechschmidt.de>
2018-02-26 14:06:01 +0100

    Added basic boilerplate using React, Relay, found and Helmet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
+import createBrowserRouter from 'found/lib/createBrowserRouter';
+import Link from 'found/lib/Link';
+import makeRouteConfig from 'found/lib/makeRouteConfig';
+import Redirect from 'found/lib/Redirect';
+import Route from 'found/lib/Route';
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function LinkItem(props) {
+  return (
+    <li>
+      <Link {...props} activeStyle={{ fontWeight: 'bold' }} />
+    </li>
+  );
+}
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+function App({ children }) {
+  return (
+    <div>
+      <ul>
+        <LinkItem to="/">Main</LinkItem>
+        <ul>
+          <LinkItem to="/foo">Foo</LinkItem>
+          <LinkItem to="/bar">Bar (async)</LinkItem>
+          <LinkItem to="/baz">Baz (redirects to Foo)</LinkItem>
+          <LinkItem to="/qux">Qux (missing)</LinkItem>
+        </ul>
+      </ul>
+
+      {children}
+    </div>
+  );
+}
+
+App.propTypes = propTypes;
+
+const BrowserRouter = createBrowserRouter({
+  routeConfig: makeRouteConfig(
+    <Route path="/" Component={App}>
+      <Route Component={() => <div>Main</div>} />
+      <Route path="foo" Component={() => <div>Foo</div>} />
+      <Route
+        path="bar"
+        getComponent={() =>
+          new Promise(resolve => {
+            setTimeout(resolve, 1000, ({ data }) => <div>{data}</div>);
+          })
+        }
+        getData={() =>
+          new Promise(resolve => {
+            setTimeout(resolve, 1000, 'Bar');
+          })
+        }
+        render={({ Component, props }) =>
+          Component && props ? (
+            <Component {...props} />
+          ) : (
+            <div>
+              <small>Loading&hellip;</small>
+            </div>
+          )
+        }
+      />
+      <Redirect from="baz" to="/foo" />
+    </Route>,
+  ),
+
+  renderError: (
+    { error }, // eslint-disable-line react/prop-types
+  ) => <div>{error.status === 404 ? 'Not found' : 'Error'}</div>,
+});
+
+ReactDOM.render(<BrowserRouter />, document.getElementById('root'));
